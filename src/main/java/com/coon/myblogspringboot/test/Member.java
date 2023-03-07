aa085be3f07a32388ecdb186c1e426489ff7f143
Coon <gongan1556@naver.com>
2023-03-07 16:09:33 +0900

    lombok Test - getter, setter, @Data, @Builder @RequireArgsConstructor...등등

--- src/main/java/com/coon/myblogspringboot/test/Member.java
+++ src/main/java/com/coon/myblogspringboot/test/Member.java
@@ -1,5 +1,9 @@
 package com.coon.myblogspringboot.test;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
 public class Member {
 
     private int id;
@@ -7,41 +11,11 @@ public class Member {
     private String password;
     private String email;
 
+    @Builder
     public Member(int id, String username, String password, String email) {
         this.id = id;
         this.username = username;
         this.password = password;
-        this.email = email;
-    }
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
         this.email = email;
     }
 }
