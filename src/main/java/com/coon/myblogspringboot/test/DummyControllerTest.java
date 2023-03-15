562505e243f8607f79703b7105b1b62832217613
Coon <gongan1556@naver.com>
2023-03-15 11:26:55 +0900

    save함수를 활용한 update 테스트

--- src/main/java/com/coon/myblogspringboot/test/DummyControllerTest.java
+++ src/main/java/com/coon/myblogspringboot/test/DummyControllerTest.java
@@ -8,10 +8,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.function.Supplier;
@@ -20,6 +18,30 @@ import java.util.function.Supplier;
 public class DummyControllerTest {
     @Autowired //의존성 주입(DI)
     private UserRepository userRepository;
+
+    //save함수는 id를 전달하지 않으면 insert 실행,
+    //id 전달시 id에 대한 데이터가 있으면 update 실행,
+    //id 전달시 id에 대한 데이터가 없으면 insert 실행.
+
+    @Transactional
+    @PutMapping("/dummy/user/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User requestUser){ // json -> Java Object로 변환
+        System.out.println("id: " + id);
+        System.out.println("password: " + requestUser.getPassword());
+        System.out.println("email: " + requestUser.getEmail());
+
+        User user = userRepository.findById(id).orElseThrow(()->{
+            return new IllegalStateException("업데이트에 실패하였습니다.");
+        });
+        user.setPassword(requestUser.getPassword());
+        user.setEmail(requestUser.getEmail());
+
+//        userRepository.save(user);
+
+        //더티 체킹
+        return null;
+    }
+
 
     @GetMapping("/dummy/users")
     public List<User> list(){
