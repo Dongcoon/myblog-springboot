b0e8a47d892c77ea400450a467d55976d022eaa9
Coon <gongan1556@naver.com>
2023-03-20 21:05:32 +0900

    Spring Security - 글 삭제하기

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -3,6 +3,9 @@ let index = {
         $("#btn-save").on("click", () => {
             this.save();
         });
+        $("#btn-delete").on("click", () => {
+            this.deleteById();
+        });
     },
 
     save: function(){
@@ -18,6 +21,23 @@ let index = {
             dataType: "json"
         }).done(function (resp){
             alert("글쓰기가 완료되었습니다.");
+            location.href = "/";
+        }).fail(function (error){
+            alert(JSON.stringify(error));
+
+        }); // ajax 통신을 이용해서 3개의 데이터를 json 으로 변경하여 insert 요청
+    },
+
+    deleteById: function(){
+        var id = $("#id").text();
+
+        $.ajax({
+            type:"DELETE",
+            url: "/api/board/"+id,
+            dataType: "json",
+            contentType: "application/json; charset=utf-8"
+        }).done(function (resp){
+            alert("글삭제가 완료되었습니다.");
             location.href = "/";
         }).fail(function (error){
             alert(JSON.stringify(error));
