9d9efbb55801becdcb9470e8d6e0461eb9efda81
Coon <gongan1556@naver.com>
2023-03-20 10:22:17 +0900

    Security 셋팅 전 주소정리(추가)

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -3,9 +3,6 @@ let index = {
         $("#btn-save").on("click", () => { // function(){}, ()=>{} this를 바인딩하기 위해서
             this.save();
         });
-        $("#btn-login").on("click", () => { // function(){}, ()=>{} this를 바인딩하기 위해서
-            this.login();
-        });
     },
 
     save: function(){
@@ -20,7 +17,7 @@ let index = {
         $.ajax({
             //회원가입 수행 요청
             type:"POST",
-            url: "/api/user",
+            url: "/auth/joinProc",
             data: JSON.stringify(data), //http body데이터
             contentType: "application/json; charset=utf-8", //body데이터가 어떤 타입인지(MIME)
             dataType: "json" //요청을 서버로해서 응답이 왔을 때 모든 것이 문자열(생긴게 json) => javascript오브젝트로 변경
@@ -32,30 +29,8 @@ let index = {
             alert(JSON.stringify(error));
 
         }); // ajax 통신을 이용해서 3개의 데이터를 json 으로 변경하여 insert 요청
-    },
-
-    login: function(){
-        // alert('user의 save함수 호출됨');
-        let data = {
-            username: $("#username").val(),
-            password: $("#password").val(),
-        };
-        $.ajax({
-            //회원가입 수행 요청
-            type:"POST",
-            url: "/api/user/login",
-            data: JSON.stringify(data), //http body데이터
-            contentType: "application/json; charset=utf-8", //body데이터가 어떤 타입인지(MIME)
-            dataType: "json" //요청을 서버로해서 응답이 왔을 때 모든 것이 문자열(생긴게 json) => javascript오브젝트로 변경
-        }).done(function (resp){
-            alert("로그인이 완료되었습니다.");
-            // console.log(resp)
-            location.href = "/";
-        }).fail(function (error){
-            alert(JSON.stringify(error));
-
-        }); // ajax 통신을 이용해서 3개의 데이터를 json 으로 변경하여 insert 요청
     }
+
 }
 
 index.init();
