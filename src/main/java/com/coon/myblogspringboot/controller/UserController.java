8a93afc308ebf110e80e31e624762a0dec48c2ee
Coon <gongan1556@naver.com>
2023-03-20 10:19:58 +0900

    Security 셋팅 전 주소정리

--- src/main/java/com/coon/myblogspringboot/controller/UserController.java
+++ src/main/java/com/coon/myblogspringboot/controller/UserController.java
@@ -4,16 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+// 인증이 안된 사용자들이 출입할 수 있는 경로를 /auth/** 허용
+// 그냥 주소가 / 이면 index.jsp 허용
+// static이하에 있는 /js/**, /css/**, /image/** 허용
 @Controller
 public class UserController {
     @GetMapping("/joinForm")
     public String joinForm(){
-        return "user/joinForm";
+        return "/auth/user/joinForm";
     }
 
     @GetMapping("/loginForm")
     public String loginForm(){
-        return "user/loginForm";
+        return "/auth/user/loginForm";
     }
 
 }
--- src/main/java/com/coon/myblogspringboot/repository/UserRepository.java
+++ src/main/java/com/coon/myblogspringboot/repository/UserRepository.java
@@ -8,11 +8,15 @@ import org.springframework.data.jpa.repository.Query;
 //자동으로 bean등록이 된다.
 //@Repository 생략 가능
 public interface UserRepository extends JpaRepository<User, Integer> {
-    //**JPA Naming전략**
-    //Select * from user where username = ? and password = ?;
-    User findByUsernameAndPassword(String username, String password);
+
+
+}
+
+
+
+//**JPA Naming전략**
+//Select * from user where username = ? and password = ?;
+//    User findByUsernameAndPassword(String username, String password);
 
 //    @Query(value = "Select * from user where username = ? and password = ?", nativeQuery = true)
 //    User login(String username, String password);
-
-}
