ff286cec6ee8c50600d64f453fb7feddf81dc607
Coon <gongan1556@naver.com>
2023-03-20 15:41:07 +0900

    Spring Security - 로그인 완료

--- src/main/java/com/coon/myblogspringboot/config/SecurityConfig.java
+++ src/main/java/com/coon/myblogspringboot/config/SecurityConfig.java
@@ -2,10 +2,7 @@ package com.coon.myblogspringboot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,7 +12,11 @@ public class SecurityConfig{
     @Bean
     public BCryptPasswordEncoder encodePWD(){
         return new BCryptPasswordEncoder();
-    };
+    }
+
+    // 시큐리티가 대신 로그인해주는데 password를 가로채기를 하는데
+    // 해당 password가 뭘 해쉬가 되어 회원가입이 되었는지 알아야
+    // 같은 해쉬로 암호화해서 DB에 있는 해쉬랑 비교할 수 있음.
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -28,7 +29,9 @@ public class SecurityConfig{
                     .authenticated()
                 .and()
                     .formLogin()
-                    .loginPage("/auth/loginForm");
+                    .loginPage("/auth/loginForm")
+                    .loginProcessingUrl("/auth/loginProc")
+                    .defaultSuccessUrl("/"); //스프링 시큐리티가 해당 주소로 요청오는 로그인을 가로채서 대신 로그인해준다.
 
         return http.build();
     }
